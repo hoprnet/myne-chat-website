cf7e1e86a07f0e08738dc4f49ffc77d07484466f
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-01-26 21:44:04 -0600

    Add loading animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
-import "../styles/main.scss";
-import { ToastProvider } from "react-toast-notifications";
+import { useState, useEffect } from 'react';
+import { ToastProvider } from 'react-toast-notifications';
+import Loader from '../components/atoms/loader';
+
+import '../styles/main.scss';
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
+  const onFinishLoading = () => {
+    setLoading(false);
+  };
+  useEffect(() => {
+    window.addEventListener('load', onFinishLoading);
+    return () => window.removeEventListener('load', onFinishLoading);
+  }, []);
   return (
     <ToastProvider>
-      <Component {...pageProps} />
+      {loading && <Loader />}
+      <div className="fadeIn">
+        <Component {...pageProps} />
+      </div>
     </ToastProvider>
   );
 }
